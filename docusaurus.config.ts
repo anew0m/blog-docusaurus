13980038f9c4c774c6bdaa52fc2d8aa21ee73aeb
anew0m <anew0m@gmail.com>
2025-01-27 13:34:26 +0900

    chore: 다국어(ko, en, jp, fr, es) 설정
    - Default: ko-한국어
    - en-영어, jp-일본어, fr-불어, es-스페인어

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -31,7 +31,7 @@ const config: Config = {
   // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'ko',
-    locales: ['ko', 'en', 'jp'],
+    locales: ['ko', 'en', 'jp', 'fr', 'es'],
     localeConfigs: {
       'ko': {
         label: 'ko-한국어',
